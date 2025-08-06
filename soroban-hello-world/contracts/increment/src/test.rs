7bc725985dd61b66cc9db69b8fdc041d6ada9a74
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-08-06 13:39:46 -0600

    finished #3 and #4, git bugged out

---
+++ soroban-hello-world/contracts/increment/src/test.rs
@@ -0,0 +1,21 @@
+#![cfg(test)]
+use crate::{IncrementContract, IncrementContractClient};
+use soroban_sdk::Env;
+
+#[test]
+fn test() {
+    let env = Env::default();
+    let contract_id = env.register(IncrementContract, ());
+    let client = IncrementContractClient::new(&env, &contract_id);
+
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.increment(), 3);
+    assert_eq!(client.decrement(), 2);
+    assert_eq!(client.decrement(), 1);
+    assert_eq!(client.decrement(), 0);
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.reset(), 0);
+
+}
